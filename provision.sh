78902c5ca9715d5cffa0c13069b4c08c618b5da4
Jeff Knecht <jeff.knecht@slalom.com>
2014-06-11 17:00:18 -0500

    Initial commit. Jenkins and Docker

---
+++ provision.sh
@@ -0,0 +1,11 @@
+# Jenkins
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins
+
+# Docker
+sudo apt-get -y install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+
