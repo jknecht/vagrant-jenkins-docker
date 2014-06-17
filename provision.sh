5bbd88b6690dd5d158c710a662ccead50d432a8e
Jeff Knecht <jeff.knecht@slalom.com>
2014-06-17 09:59:37 -0500

    add docker support

--- provision.sh
+++ provision.sh
@@ -4,8 +4,15 @@ sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources
 sudo apt-get update
 sudo apt-get -y install jenkins
 
+# Maven
+sudo apt-get -y install openjdk-7-jdk
+sudo apt-get -y install maven
+
 # Docker
 sudo apt-get -y install docker.io
 sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 
+sudo groupadd docker
+sudo gpasswd -a jenkins docker
+sudo service docker.io restart
